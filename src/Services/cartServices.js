8357e7f3f981dcea2ae45a6d8cdc7231b0b96e7c
mohamed-wassal <mohamedwassal444@gmail.com>
2025-02-22 17:56:42 +0200

    first commit

---
+++ src/Services/cartServices.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { Bounce, toast } from "react-toastify";
+
+
+
+
+
+
+export async function addProductToCart(productId,setIsLoading) {
+    setIsLoading(true)
+    const { data } = await axios.post("https://ecommerce.routemisr.com/api/v1/cart",{
+      productId 
+    },{
+      headers: {
+        token: localStorage.getItem("token"),
+      }
+    })
+   
+    setIsLoading(false)
+    if (data.status == "success"){
+      toast.success(data.message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        style: {
+          maxWidth: "300px", 
+          width: "90%",
+          marginTop: "5%", 
+        },
+        });
+    }
+    
+  }
---
+++ src/Services/wishlistServices.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { Bounce, toast } from "react-toastify";
+
+export   async function addProductToWishlist(productId , setWishlistLoading) {
+    setWishlistLoading(true)
+    const { data } = await axios.post("https://ecommerce.routemisr.com/api/v1/wishlist",{
+      productId
+    },{
+      headers:{
+        token: localStorage.getItem("token")
+      }
+    })
+
+    setWishlistLoading(false)
+    if (data.status == "success"){
+      toast.success(data.message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        style: {
+          maxWidth: "300px", 
+          width: "90%", 
+          marginTop: "5%", 
+        },
+        });
+    }
+    
+  }
